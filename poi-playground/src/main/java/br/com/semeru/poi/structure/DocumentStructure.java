3fc29e1477b3a80698fb1941a8b1ef344e707100
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-05-19 13:13:14 -0300

    Moving code to Travis CI

--- poi-playground/src/main/java/br/com/semeru/poi/structure/DocumentStructure.java
+++
@@ -1,8 +0,0 @@
-package br.com.semeru.poi.structure;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class DocumentStructure {
-	public List<SheetStructure> sheetStructures = new ArrayList<SheetStructure>(); 
-}
